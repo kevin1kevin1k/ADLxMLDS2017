b089223f1fd92f6e94e396ad816b99f138fcaffc
Ubuntu <kevin1kevin1k@adl.20rovo3oe35uhpis1t2kem3asa.bx.internal.cloudapp.net>
2017-11-12 05:12:40 +0000

    modify model path

--- hw1/model_rnn.py
+++ hw1/model_rnn.py
@@ -8,12 +8,12 @@ CSV_FILENAME = sys.argv[2]
 '''
 from train import *
 build_data(DATA_PATH)
-train_rnn('../models/')
+train_rnn('./models/')
 '''
 
 from predict import *
 
 read_maps(DATA_PATH)
 build_data_test(DATA_PATH)
-y_predict = predict_single_model('../models/model_loss_015_0.0454_mapafter_lstm4_dp0.2_hidden500_adam0.0005.h5')
+y_predict = predict_single_model('./models/model_loss_015_0.0454_mapafter_lstm4_dp0.2_hidden500_adam0.0005.h5')
 write_csv(y_predict, CSV_FILENAME)
